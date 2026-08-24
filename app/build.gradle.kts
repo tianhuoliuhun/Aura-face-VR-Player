1bdab0d57da30078b1b1b7d922db67fc502a65a3
tianhuoliuhun <tianhuoliuhun@gmail.com>
2026-08-24 10:39:44 +0800

    v113: 版本升至1.0.113 + 音频解码器软件回退

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.aistudio.vrplayer.vrmjpy"
     minSdk = 24
     targetSdk = 36
-    versionCode = 112
-    versionName = "1.0.112"
+    versionCode = 113
+    versionName = "1.0.113"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
